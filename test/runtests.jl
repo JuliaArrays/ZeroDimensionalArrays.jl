81dc08837c025fe0fa360f684d37d63d4ea7e49c
Neven Sajko <s@purelymail.com>
2025-04-10 09:02:18 +0200

    add tests for max code coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,53 @@ using Aqua: Aqua
     @testset "Code quality (Aqua.jl)" begin
         Aqua.test_all(ZeroDimensionalArrays)
     end
-    # Write your tests here.
+
+    @testset "all array types joined" begin
+        for Arr ∈ (
+            ZeroDimensionalArrayImmutable,
+            ZeroDimensionalArrayMutable,
+            ZeroDimensionalArrayMutableConstField,
+        )
+            @test isstructtype(Arr)
+            @test Arr <: AbstractArray{<:Any, 0}
+            @test (@inferred Arr(fill(0.3))) isa Arr{Float64}
+            @test (@inferred convert(Arr, fill(0.3))) isa Arr{Float64}
+            @test (@inferred Arr{Float32}(fill(0.3))) isa Arr{Float32}
+            @test (@inferred convert(Arr{Float32}, fill(0.3))) isa Arr{Float32}
+            @test () === @inferred propertynames(Arr(fill(0.3)))
+            @test only(fill(0.3)) === @inferred only(Arr(fill(0.3)))
+            @test fill(0.3)[] === @inferred Arr(fill(0.3))[]
+            @test fill(0.3)[1] === @inferred Arr(fill(0.3))[1]
+            @test @inferred isassigned(Arr(fill(0.3)))
+            @test @inferred isassigned(Arr(fill(0.3)), 1)
+            @test !(isassigned(Arr(fill(0.3)), 2))
+            @test (@inferred similar(Arr(fill(0.3)), Float32)) isa ZeroDimensionalArrayMutable{Float32}
+            @test fill(0.3) == Arr(fill(0.3))
+            @test Arr(fill(0.3)) == Arr(fill(0.3))
+            @test all(@inferred Arr(fill(0.3)) .== Arr(fill(0.3)))
+            @test (@inferred Arr(fill(0.3)) .+ [10, 20]) isa AbstractVector
+        end
+    end
+
+    @testset "each array type on its own" begin
+        @testset "`ZeroDimensionalArrayImmutable`" begin
+            @test @isdefined ZeroDimensionalArrayImmutable
+            @test !ismutabletype(ZeroDimensionalArrayImmutable)
+            @test isbitstype(ZeroDimensionalArrayImmutable{Float64})
+        end
+        @testset "`ZeroDimensionalArrayMutable`" begin
+            @test @isdefined ZeroDimensionalArrayMutable
+            @test ismutabletype(ZeroDimensionalArrayMutable)
+            @test (@inferred ZeroDimensionalArrayMutable{Float32}(undef)) isa ZeroDimensionalArrayMutable{Float32}
+            @test (@inferred ZeroDimensionalArrayMutable{Float32}(undef, ())) isa ZeroDimensionalArrayMutable{Float32}
+            @test let a = ZeroDimensionalArrayMutable(fill(0.3))
+                a[] = 0.7
+                only(a) === 0.7
+            end
+        end
+        @testset "`ZeroDimensionalArrayMutableConstField`" begin
+            @test @isdefined ZeroDimensionalArrayMutableConstField
+            @test ismutabletype(ZeroDimensionalArrayMutableConstField)
+        end
+    end
 end
