bea85b1b39420420f238aa6013b8aae6a74e7fcc
Neven Sajko <s@purelymail.com>
2025-04-12 10:51:36 +0200

    test: deduplicate code

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,42 +8,43 @@ using Aqua: Aqua
     end
 
     @testset "all array types joined" begin
+        x = 0.3
         for Arr ∈ (
             ZeroDimArray,
             Box,
             BoxConst,
         )
-            @test (@inferred Arr(0.3)) == (@inferred convert(Arr, fill(0.3)))
+            @test (@inferred Arr(x)) == (@inferred convert(Arr, fill(x)))
             @test isstructtype(Arr)
             @test Arr <: AbstractArray{<:Any, 0}
-            @test (@inferred Arr(0.3)) isa Arr{Float64}
-            @test (@inferred convert(Arr, fill(0.3))) isa Arr{Float64}
-            @test (@inferred Arr{Float32}(0.3)) isa Arr{Float32}
-            @test (@inferred convert(Arr{Float32}, fill(0.3))) isa Arr{Float32}
-            @test () === @inferred propertynames(Arr(0.3))
-            @test only(fill(0.3)) === @inferred only(Arr(0.3))
-            @test fill(0.3)[] === @inferred Arr(0.3)[]
-            @test fill(0.3)[1] === @inferred Arr(0.3)[1]
-            @test @inferred isassigned(Arr(0.3))
-            @test @inferred isassigned(Arr(0.3), 1)
-            @test !(isassigned(Arr(0.3), 2))
-            @test (@inferred iterate(Arr(0.3))) isa Tuple{Float64, Any}
-            @test 0.3 === first(iterate(Arr(0.3)))
-            @test let a = Arr(0.3)
+            @test (@inferred Arr(x)) isa Arr{typeof(x)}
+            @test (@inferred convert(Arr, fill(x))) isa Arr{typeof(x)}
+            @test (@inferred Arr{Float32}(x)) isa Arr{Float32}
+            @test (@inferred convert(Arr{Float32}, fill(x))) isa Arr{Float32}
+            @test () === @inferred propertynames(Arr(x))
+            @test only(fill(x)) === @inferred only(Arr(x))
+            @test fill(x)[] === @inferred Arr(x)[]
+            @test fill(x)[1] === @inferred Arr(x)[1]
+            @test @inferred isassigned(Arr(x))
+            @test @inferred isassigned(Arr(x), 1)
+            @test !(isassigned(Arr(x), 2))
+            @test (@inferred iterate(Arr(x))) isa Tuple{typeof(x), Any}
+            @test x === first(iterate(Arr(x)))
+            @test let a = Arr(x)
                 nothing === @inferred iterate(a, last(iterate(a)))
             end
-            @test nothing === @inferred iterate(Arr(0.3), nothing)
-            @test (@inferred similar(Arr(0.3))) isa Box{Float64}
-            @test (@inferred similar(Arr(0.3), Float32)) isa Box{Float32}
-            @test (@inferred copy(Arr(0.3))) isa Arr{Float64}
-            @test (@inferred copy(Arr{AbstractFloat}(0.3))) isa Arr{AbstractFloat}
-            @test let a = Arr(0.3)
+            @test nothing === @inferred iterate(Arr(x), nothing)
+            @test (@inferred similar(Arr(x))) isa Box{typeof(x)}
+            @test (@inferred similar(Arr(x), Float32)) isa Box{Float32}
+            @test (@inferred copy(Arr(x))) isa Arr{typeof(x)}
+            @test (@inferred copy(Arr{AbstractFloat}(x))) isa Arr{AbstractFloat}
+            @test let a = Arr(x)
                 a == copy(a)
             end
-            @test fill(0.3) == Arr(0.3)
-            @test Arr(0.3) == Arr(0.3)
-            @test all(@inferred Arr(0.3) .== Arr(0.3))
-            @test (@inferred Arr(0.3) .+ [10, 20]) isa AbstractVector
+            @test fill(x) == Arr(x)
+            @test Arr(x) == Arr(x)
+            @test all(@inferred Arr(x) .== Arr(x))
+            @test (@inferred Arr(x) .+ [10, 20]) isa AbstractVector
         end
     end
 
